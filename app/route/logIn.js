cd81f2dc20085287c9705f22b0d29e4ec2819150
Aak-la <486910173@qq.com>
2022-07-04 16:33:44 +0800

    注册登录逻辑跑通

--- app/route/logIn.js
+++ app/route/logIn.js
@@ -4,30 +4,32 @@ const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const Admin = require("../models/admin");
 router.post("/", (req, res) => {
-  let name = req.query.username;
-  let password = req.query.password;
+  let name = req.body.params.name;
+  let password = req.body.params.passWord;
   let md5 = crypto.createHash("md5");
-  let newPassword = md5.update(password).digest("hex");  
+  let newPassword = md5.update(password).digest("hex");
   Admin.findAll({
-      where: {
-          username: name
-      },
-      raw: true
-  }).then(function(data){
-      if(data){
-          if(data[0].password === newPassword){
-              let token = jwt.sign(data[0],'app.get(superSecret)',{
-                  expiresIn: 1440 
-              })
-              res.send({
-                  success: true,
-                  message: 'Enjoy your token!',
-                  token: token
-              });
-          }else{
-              res.send("登录失败!")
-          }
+    where: {
+      username: name,
+    },
+    raw: true,
+  }).then(function (data) {
+    if (data) {
+      if (data[0].password === newPassword) {
+        let token = jwt.sign(data[0], "app.get(superSecret)", {
+          expiresIn: 1440,
+        });
+        res.send({
+          code: 0,
+          data: [{ msg: "登录成功", token: token }],
+        });
+      } else {
+        res.send({
+            code: 500,
+            data: [{ msg: "登录失败" }],
+          });
       }
-  })
+    }
+  });
 });
 module.exports = router;
--- app/route/register.js
+++ app/route/register.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const crypto = require("crypto");
 const Admin = require("../models/admin");
 router.post("/", (req, res) => {
-  let name = req.query.username;
-  let password = req.query.password;
+  let name = req.body.params.name;
+  let password = req.body.params.passWord;
   let md5 = crypto.createHash("md5");
   let newPassword = md5.update(password).digest("hex");
   Admin.findAll({
@@ -17,18 +17,27 @@ router.post("/", (req, res) => {
       return data;
     })
     .then((data) => {
-      if (data) {
-        res.send("已存在用户");
+      if (data.length) {
+        res.send({
+          code: 0,
+          data: [{ msg: "用户已存在，重新输入" }],
+        });
       } else {
         Admin.create({
           username: name,
           password: newPassword,
         })
           .then(function (result) {
-            res.send("注册成功!");
+            res.send({
+              code: 0,
+              data: [{ msg: "注册成功!" }],
+            });
           })
           .catch(function (err) {
-            res.send("注册失败!" + err);
+            res.send({
+              code: 0,
+              data: [{ msg: "注册失败!" }],
+            });
           });
       }
     });
